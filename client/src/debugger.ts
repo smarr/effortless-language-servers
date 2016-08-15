1b38dcf277108ab6c1de94b90ba1dbc434b8aa62
Stefan Marr <git@stefan-marr.de>
2016-08-15 16:53:11 +0200

    Implement setting of breakpoints
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- client/src/debugger.ts
+++ client/src/debugger.ts
@@ -1,6 +1,7 @@
 "use strict";
 
-import { DebugSession, InitializedEvent }  from 'vscode-debugadapter';
+import { DebugSession, InitializedEvent, BreakpointEvent, StoppedEvent }
+  from 'vscode-debugadapter';
 import { DebugProtocol } from 'vscode-debugprotocol';
 import * as WebSocket from 'ws';
 
@@ -26,11 +27,48 @@ export interface AttachRequestArguments extends DebugProtocol.AttachRequestArgum
   port: number
 }
 
+namespace WebDebugger {
+  export class Breakpoint {
+    public type:      string;
+    public sourceUri: string;
+    public enabled:   boolean;
+
+    constructor(type: string, uri: string, enabled: boolean) {
+      this.type      = type;
+      this.sourceUri = uri;
+      this.enabled   = enabled;
+    }
+  }
+
+  export class LineBreakpoint extends Breakpoint {
+    public line: number;
+
+    constructor(uri: string, enabled: boolean, line: number) {
+      super("lineBreakpoint", uri, enabled);
+      this.line = line;
+    }
+  }
+}
+
+interface BreakpointPair {
+  vs: DebugProtocol.Breakpoint;
+  som: WebDebugger.Breakpoint;
+}
+
 class SomDebugSession extends DebugSession {
   private socket: WebSocket;
+  private breakpoints: BreakpointPair[];
+  private nextBreakpointId : number;
+  private bufferedBreakpoints : WebDebugger.Breakpoint[];
 
   public constructor() {
     super();
+
+    this.socket = null;
+
+    this.breakpoints = [];
+    this.bufferedBreakpoints = [];
+    this.nextBreakpointId = 0;
 
     // Truffle source sections use 1-based indexes
     this.setDebuggerColumnsStartAt1(true);
@@ -61,14 +99,85 @@ class SomDebugSession extends DebugSession {
     this.socket = new WebSocket('ws://localhost:' + args.port);
 
     this.socket.on('open', () => {
-      this.socket.send(JSON.stringify({
-        action: "initialBreakpoints", breakpoints: []}));
+      this.sendInitialBreakpoints();
     });
+
+    this.socket.onmessage = this.onWDMessage;
     
+    this.sendResponse(response);
+
     // TODO:
-    // - start with support for attaching to the existing WebDebugger
     // - implement setting breakpoints
     // - implement stepping 
+  }
+
+  private sendInitialBreakpoints() : void {
+    console.log("sendInitialBreakpoints: " + JSON.stringify(this.bufferedBreakpoints));
+    const updatedBreakpoints = [];
+    const initialBreakpoints = [];
+    
+    for (const id in this.bufferedBreakpoints) {
+      console.log("sendInitialBreakpoints id: " + id);
+      const vsBp = this.breakpoints[id].vs;
+      vsBp.verified = true;
+      updatedBreakpoints.push(vsBp);
+      initialBreakpoints.push(this.bufferedBreakpoints[id]);
+    }
+    const msg = JSON.stringify({
+        action: "initialBreakpoints", breakpoints: initialBreakpoints});
+    this.socket.send(msg);
+    console.log("Sent initialBreakpoints message: " + msg);
+    
+    for (const vsBp of updatedBreakpoints) {
+      this.sendEvent(new BreakpointEvent('Connection Established', vsBp));
+    }
+  }
+
+  protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse,
+      args: DebugProtocol.SetBreakpointsArguments): void {
+    console.log("setBreakPointsRequest: " + JSON.stringify(args));
+
+    const connected = this.socket != null;
+    const uri = "file:" + args.source.path;
+    const breakpoints = [];
+    response.body = {breakpoints: breakpoints};
+
+    for (let bp of args.breakpoints) {
+      const bpId = this.getNextBpId();
+      const lineBp = new WebDebugger.LineBreakpoint(uri, true, bp.line);
+      this.sendBreakpoint(lineBp, bpId, connected);
+      
+      const vsBp = {
+        id:       bpId,
+        verified: connected,
+        source:   args.source,
+        line:     bp.line
+      }
+      this.breakpoints[vsBp.id] = {vs: vsBp, som: lineBp};
+
+      breakpoints.push(vsBp);
+    }
+    this.sendResponse(response);
+  }
+
+  private getNextBpId(): number {
+    const id = this.nextBreakpointId;
+    this.nextBreakpointId += 1;
+    return id;
+  }
+
+  private sendBreakpoint(bp: WebDebugger.Breakpoint, bpId: number,
+      connected: boolean): void {
+    if (connected) {
+      console.log("sendBreakpoint: " + JSON.stringify(bp));
+      this.socket.send(JSON.stringify({
+        action: "updateBreakpoint",
+        breakpoint: bp
+      }));
+    } else {
+      console.log("Buffer breakpoint: " + JSON.stringify(bp));
+      this.bufferedBreakpoints[bpId] = bp;
+    }
   }
 }
 
