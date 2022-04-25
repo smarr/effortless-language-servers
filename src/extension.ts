20592fe4731d00a29feafebb4d631577a2ca93af
Stefan Marr <git@stefan-marr.de>
2022-04-25 01:20:49 +0100

    Try to find the java command using the shell
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,7 @@ let serverProcess: ChildProcess = null;
 
 function getServerOptions(asAbsolutePath: PathConverter, enableDebug:
 	  boolean, enableTcp: boolean): ServerOptions {
-	const javaCmd = '/usr/bin/java';
+	const javaCmd = 'java';
 
   const javaClassPath = [
 		asAbsolutePath('out/server/som.jar'),
@@ -77,7 +77,7 @@ function startLanguageServerAndConnectTCP(context: ExtensionContext,
 													   resolve: (value?: StreamInfo | PromiseLike<StreamInfo>) => void,
 														 reject: (reason?: any) => void): void {
 	const serverOptions: any = getServerOptions(context.asAbsolutePath, true, true);
-	const lsProc = spawn(serverOptions.run.command, serverOptions.run.args);
+	const lsProc = spawn(serverOptions.run.command, serverOptions.run.args, {shell: true});
 	let sawServerStarted = false;
 	lsProc.stdout.on('data', data => {
 		if (!sawServerStarted && data.toString().includes('Server started and waiting')) {
@@ -102,7 +102,7 @@ export function startLanguageServer(asAbsolutePath: PathConverter,
 
 	const cmd = `${serverOptions.run.command} ${serverOptions.run.args.join(" ")}`;
 	console.log(`[SOM-EXT] spawn '${cmd}'`);
-	const lsProc = spawn(serverOptions.run.command, serverOptions.run.args);
+	const lsProc = spawn(serverOptions.run.command, serverOptions.run.args, {shell: true});
 
 	let stderr = '';
 
